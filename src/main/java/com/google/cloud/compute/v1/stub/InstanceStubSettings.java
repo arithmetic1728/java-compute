321c7836c91be093267cd2695c4494c9c00e880d
arithmetic1728 <liusijun1985@gmail.com>
2021-02-15 01:54:42 -0800

    feat: use self signed jwt from auth lib

--- src/main/java/com/google/cloud/compute/v1/stub/InstanceStubSettings.java
+++ src/main/java/com/google/cloud/compute/v1/stub/InstanceStubSettings.java
@@ -492,7 +492,7 @@ public class InstanceStubSettings extends StubSettings<InstanceStubSettings> {
 
   /** Returns a builder for the default credentials for this service. */
   public static GoogleCredentialsProvider.Builder defaultCredentialsProviderBuilder() {
-    return GoogleCredentialsProvider.newBuilder().setScopesToApply(DEFAULT_SERVICE_SCOPES);
+    return GoogleCredentialsProvider.newBuilder().setDefaultScopes(DEFAULT_SERVICE_SCOPES);
   }
 
   /** Returns a builder for the default ChannelProvider for this service. */
